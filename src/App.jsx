c2522c7e81ffe8e30da2fb7cbd4c69d582d27b1c
JulianAballay <juliancabj1210@gmail.com>
2024-01-15 23:57:38 -0300

    Agrego maquetado de logo simpsons y boton en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Button, Container } from 'react-bootstrap'
-import './App.css'
+import { Button, Container, Image } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-
-
   return (
-    <Container>
-      <Button></Button>
+    <Container className="text-center">
+      <section>
+        <Image src="/src/assets/logoSimpsons.png" className="w-50" />
+      </section>
+      <Button variant="warning">Obtener frase</Button>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
