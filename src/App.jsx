09dfb1530d419eaceb36c915dab7335f0fed7c1e
JulianAballay <juliancabj1210@gmail.com>
2024-01-16 00:20:16 -0300

    Agrego parte maquetado componente frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Button, Container, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Frase from "./components/Frase";
 
 function App() {
   return (
-    <Container className="text-center">
+    <Container className="text-center my-3">
       <section>
         <Image src="/src/assets/logoSimpsons.png" className="w-50" />
       </section>
+      <section>
+        <Frase />
+      </section>
+
       <Button variant="warning">Obtener frase</Button>
     </Container>
   );
---
+++ src/components/Frase.jsx
@@ -0,0 +1,16 @@
+import { Image } from "react-bootstrap";
+
+const Frase = () => {
+    return (
+        <>
+        <h2>
+            Moe Szyslak
+        </h2>
+        <hr />
+        <Image src="/src/assets/Moe_Szyslak.png"></Image>
+        
+        </>
+    );
+};
+
+export default Frase;
