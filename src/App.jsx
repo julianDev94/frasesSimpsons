204a8913be9ee469e4a3846ee2bffbea56e60d63
JulianAballay <juliancabj1210@gmail.com>
2024-02-15 14:58:00 -0300

    importo la ruta della imagen del logo y agrego clase width de 50%

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { Button, Container, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from './assets/logoSimpsons.png';
 import "./App.css";
 import Frase from "./components/Frase";
 
 function App() {
   return (
     <Container className="text-center my-3">
-      <section>
-        <Image src="/src/assets/logoSimpsons.png" className="w-50" />
-      </section>
-      <section>
-        <Frase />
-      </section>
-
+      <Image src={logo}lassName="w-50" />
+      <Frase />
       <Button variant="warning">Obtener frase</Button>
     </Container>
   );
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,15 +1,26 @@
-import { Image } from "react-bootstrap";
+import { Image, Card } from "react-bootstrap";
 
 const Frase = () => {
     return (
-        <>
+        <section className="my-3">
         <h2>
             Moe Szyslak
         </h2>
         <hr />
         <Image src="/src/assets/Moe_Szyslak.png"></Image>
-        
-        </>
+        <Card>
+        <Card.Body>
+          <figure>
+            <blockquote className="blockquote">
+              <p>"Shoplifting is a victimless crime, like punching someone in the dark."</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+            "Nelson Muntz" in <cite title="Source Title">The simpsons</cite>
+            </figcaption>
+          </figure>
+        </Card.Body>
+      </Card>
+        </section>
     );
 };
 
