dc2be6f7f39ebfba0fc67f133f2a4033a908e739
JulianAballay <juliancabj1210@gmail.com>
2024-02-15 21:57:20 -0300

    agrego logica de peticion a la api de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './assets/logoSimpsons.png';
 import "./App.css";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
 function App() {
+const [personaje, setPersonaje] = useState({}); 
+
+
+  useEffect(()=>{
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () =>{
+    // console.log('esta funcion se llama en montaje');
+    const consult = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes`);
+    const datos = await consult.json();
+    console.log(datos[0]);
+    setPersonaje(datos[0]); 
+  }
+
   return (
     <Container className="text-center my-3">
       <Image src={logo} alt="Imagen de titulo de simpsons" className="w-50" />
-      <Frase />
-      <Button variant="warning">Obtener frase</Button>
+      <Frase personaje={personaje} />
+      <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
     </Container>
   );
 }
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,21 +1,21 @@
 import { Image, Card } from "react-bootstrap";
-import ImagenSimpsons from "../assets/Moe_Szyslak.png"
-const Frase = () => {
+// import ImagenSimpsons from "../assets/Moe_Szyslak.png"
+const Frase = ({personaje}) => {
     return (
         <section className="my-3">
         <h2>
-            Moe Szyslak
+            {personaje.character}
         </h2>
         <hr />
-        <Image src={ImagenSimpsons}></Image>
+        <Image src={personaje.image} alt={personaje.character}></Image>
         <Card>
         <Card.Body>
           <figure>
             <blockquote className="blockquote">
-              <p>"Shoplifting is a victimless crime, like punching someone in the dark."</p>
+              <p>{personaje.quote}</p>
             </blockquote>
             <figcaption className="blockquote-footer">
-            "Nelson Muntz" in <cite title="Source Title">The simpsons</cite>
+            "{personaje.character}" in <cite title="Source Title">The simpsons</cite>
             </figcaption>
           </figure>
         </Card.Body>
